920182bda4231343d92f1d79f0689c6e4d7aac0d
rostyslav.bornitskyi <doofixxx@gmail.com>
2023-09-07 02:13:00 +0300

    init

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('cashlogix - expenses visualization');
+console.log();
+console.log('Usage:   cashlogix [options]');
+console.log();
+console.log('Examples:');
+console.log();
+console.log('   cashlogix logs.json');
