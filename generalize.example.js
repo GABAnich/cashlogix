aa853f3db43dc380709c296adc077e0e237f3d9d
rostyslav.bornitskyi <doofixxx@gmail.com>
2023-09-16 11:22:59 +0300

    prettier

--- generalize.example.js
+++ generalize.example.js
@@ -1,7 +1,7 @@
-const generalize_description = ({ description, ...log}) => {
-  if (description.includes('movie')) return { description: 'movie', ...log };
-  if (description.includes('cinema')) return { description: 'movie', ...log };
-  return { description: '', ...log };
+const generalize_description = ({ description, ...log }) => {
+  if (description.includes("movie")) return { description: "movie", ...log };
+  if (description.includes("cinema")) return { description: "movie", ...log };
+  return { description: "", ...log };
 };
 
-module.exports = generalize_description; 
+module.exports = generalize_description;
